2a228d0a636c79a94df5ee5b1c1987f5addd20c3
Mattia Leoni <mattialeoni@gmail.com>
2019-03-05 10:17:36 +0100

    Update dbhelper.js

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -9,7 +9,7 @@ class DBHelper {
    */
   static get DATABASE_URL() {
     const port = 8000 // Change this to your server port
-    return `https://localhost:${port}/data/restaurants.json`;
+    return `https://mattyleo.github.io/restaurant-review/data/restaurants.json`;
   }
 
   /**
