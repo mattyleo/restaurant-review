4597bea9634e1939a75044de2b13b0ee30abdcf5
Mattia Leoni <mattialeoni@gmail.com>
2019-03-05 10:12:06 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
 const CACHE_NAME = 'restaurant-cache-v1';
 const urlsToCache = [
   './',
-  'css/styles.css',
-  'css/responsive.css',
-  'data/restaurants.json',
-  'img/1.jpg',
-  'img/2.jpg',
-  'img/3.jpg',
-  'img/4.jpg',
-  'img/5.jpg',
-  'img/6.jpg',
-  'img/7.jpg',
-  'img/8.jpg',
-  'img/9.jpg',
-  'img/10.jpg',
-  'js/dbhelper.js',
-  'js/main.js',
-  'js/restaurant_info.js',
-  'index.html',
-  'restaurant.html'
+  '/css/styles.css',
+  '/css/responsive.css',
+  '/data/restaurants.json',
+  '/img/1.jpg',
+  '/img/2.jpg',
+  '/img/3.jpg',
+  '/img/4.jpg',
+  '/img/5.jpg',
+  '/img/6.jpg',
+  '/img/7.jpg',
+  '/img/8.jpg',
+  '/img/9.jpg',
+  '/img/10.jpg',
+  '/js/dbhelper.js',
+  '/js/main.js',
+  '/js/restaurant_info.js',
+  '/index.html',
+  '/restaurant.html'
 ];
 
 self.addEventListener('install', function(event) {
